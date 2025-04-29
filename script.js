5488125af7da0969a3b5229d28ec4eeeaf02fa68
Maldoseri23 <maldoseria@gmail.com>
2025-04-29 12:25:15 +0300

    started main game function added some constants

---
+++ script.js
@@ -0,0 +1,15 @@
+const candyCrushGame = () => {
+  const grid = document.querySelector('.grid')
+  const scoreDisplay = document.getElementById('score')
+  const width = 8
+  const squares = []
+  let score = 0
+  const candyColors = [
+    'url("./blue-candy.png")',
+    'url("./yellow-candy.png")',
+    'url("./green-candy.png")',
+    'url("./orange-candy.png")',
+    'url("./purple-candy.png")',
+    'url("./red-candy.png")'
+  ]
+}
